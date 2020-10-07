196b2f5b261df42c7e1ce10e0b485e845091710f
Luis Fernando Cruz Ramos <luisfernandocruzramos@gmail.com>
2020-10-07 18:46:16 +0200

    feat: Heimdal project creation

---
+++ heimdal_server/heimdal_backend/src/main/java/es/udc/tfm/heimdal/heimdal_server/HeimdalAppServerApplication.java
@@ -0,0 +1,13 @@
+package es.udc.tfm.heimdal.heimdal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdalAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdalAppServerApplication.class, args);
+	}
+
+}
---
+++ heimdal_server/heimdal_backend/src/test/java/es/udc/tfm/heimdal/heimdal_server/HeimdalAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.tfm.heimdal.heimdal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdalAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
